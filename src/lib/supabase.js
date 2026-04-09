26f78220d2a398ff47967bdf01e67cb0e9a46b37
Natalie25-4 <162855358+Natalie25-4@users.noreply.github.com>
2026-04-09 17:06:04 +1200

    use environment variable for supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,11 +3,13 @@
 // Description: supabase configuration file
 //creates and exports the supabase client instance
 //used to connect react application to the backened database
-import { createClient} from "@supabase/supabase-js";
-//supabase project URL
-const supabaseUrl="https://kyvhotzqnuuyntbzjkqp.supabase.co";
-//supabase public client key
-const supabaseAnonKey="sb_publishable_9LUIMyHgasO4u60g09IO8g_Lff20WPB";
 
-//export supabase client for use in other files.
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+import {createClient} from "@supabase/supabase-js";
+
+//supabase project url and public key loaded from the environment variable
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+//export supabase client for use in the other files
+export const supabase = createClient(supabaseURL, supabaseAnonKey);
